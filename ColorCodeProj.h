52057b5841a8bf86efd1fc45df557806b35c84ce
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-02-23 17:31:57 +0530

    Update ColorCodeProj.h
    optmimized header

--- ColorCodeProj.h
+++ ColorCodeProj.h
@@ -1,7 +1,7 @@
 /*Header file for project common functions */
 
 #define MAX_COLORPAIR_NAME_CHARS  16 /*Max chars allowed for color pair*/
-
+#define numberOfMinorColors 5
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET}; /*Major colour enum*/
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE}; /*Minor colour enum*/
 /*Major and Minor color names*/
