545ffcbd2f858bdd7fae033dcef9139a54110184
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-02-23 17:09:38 +0530

    Update ColorCodeProj.h
    header file improvements

--- ColorCodeProj.h
+++ ColorCodeProj.h
@@ -4,6 +4,14 @@
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET}; /*Major colour enum*/
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE}; /*Minor colour enum*/
+/*Major and Minor color names*/
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
 /*Color pair struct */
 typedef struct {
     enum MajorColor majorColor;
