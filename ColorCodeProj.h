f8df1ba030b11a865031ef1de754e45243541718
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-02-23 18:24:29 +0530

    Update ColorCodeProj.h
    coding guidelines adapted

--- ColorCodeProj.h
+++ ColorCodeProj.h
@@ -1,10 +1,17 @@
+/*********************************************/
+/***File   : ColorCodeProj.h
+/***Author : Naveen B 
+/***brief  : Color code project header file
+/*********************************************/
+
 /*Header file for project common functions */
 
 #define MAX_COLORPAIR_NAME_CHARS  16 /*Max chars allowed for color pair*/
-#define numberOfMinorColors 5
+#define NUM_OF_MINOR_COLOR  5 /*Number of Minor colors*/
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET}; /*Major colour enum*/
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE}; /*Minor colour enum*/
-/*Major and Minor color names*/
+
+/*Major and Minor color name arrays*/
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
